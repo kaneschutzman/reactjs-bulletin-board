d7ba417104a866a05815f96da457b2264fef0cdf
Maciej Kurek <maciejk77@gmail.com>
2015-07-21 20:56:37 +0200

    Adding new note, starting from empty board

--- js/Note.js
+++ js/Note.js
@@ -57,13 +57,13 @@ var Board = React.createClass({
     },
     getInitialState: function() {
         return {
-            notes: [
-                'Buy Milk',
-                'Send Postard',
-                'Clean the house',
-                'Do laundry'
-            ]
+            notes: []
         };
+    },
+    add: function(text) {
+        var arr = this.state.notes;
+        arr.push(text);
+        this.setState({notes: arr});
     },
     update: function(newText, i) {
         var arr = this.state.notes;
@@ -87,6 +87,8 @@ var Board = React.createClass({
     render: function() {
         return (<div className="board">
                     {this.state.notes.map(this.eachNote)}
+                    <button className="btn btn-sm btn-success glyphicon glyphicon-plus"
+                            onClick={this.add.bind(null, "New Note")}/>
             </div>
 
         );
