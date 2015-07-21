4d53ec9fc46ce1ad86c1cf28bd1416fc72674e13
Maciej Kurek <maciejk77@gmail.com>
2015-07-21 20:39:24 +0200

    Multiple cards, initial state, iteration over JSX with map method

--- js/Note.js
+++ js/Note.js
@@ -45,5 +45,50 @@ var Note = React.createClass({
     }
 });
 
-React.render(<Note>Hello World</Note>, 
+var Board = React.createClass({
+    propTypes: {
+        count: function(props, propName) {
+            if (typeof props[propName] !== "number"){
+                return new Error('The count property must be a number');
+            }
+            if (props[propName] > 100) {
+                return new Error("Creating " + props[propName] + " notes is ridiculous");
+            }
+        }
+    },
+    getInitialState: function() {
+        return {
+            notes: [
+                'Buy Milk',
+                'Send Postcard',
+                'Clean the house',
+                'Do laundry'
+            ]
+        };
+    },
+    render: function() {
+        return (<div className="board">
+                    {this.state.notes.map(function(note, i){
+                        return (
+                            <Note key={i}>{note}</Note>
+                        );
+                    })}
+            </div>
+
+        );
+    }
+});
+
+
+React.render(<Board count={10}/>, 
     document.getElementById('react-container'));
+
+
+
+
+
+
+
+
+
+
